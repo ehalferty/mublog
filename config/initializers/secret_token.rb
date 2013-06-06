81e22d8e4542787a479ebe275facb13a42ebd156
Ed Halferty <edhalferty@gmail.com>
2013-06-06 04:11:01 -0400

    Initial commit, default rails folder.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mublog::Application.config.secret_token = '0a5a1c23ad209f5d1c410b6bc4a7598024a301ff60f9e6a754c79a7ca26552c399e2488a410d50e7b80da6d9d8ba7d2016b16b90ca9033e629326333fe78e8de'
